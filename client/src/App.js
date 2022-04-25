b972812c4faefb6c55991133aa7a2ac4758c7914
Dima <benensondimitrii@gmail.com>
2022-04-25 15:03:52 +0300

    fetched data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { mockData } from "./assets/mockData";
 import MainPage from "./pages/MainPage";
 
 import "./App.css";
+import { getData } from "./lib/api";
 
 function App() {
-  return <div className="App">
-    <MainPage mockData={mockData}></MainPage>
-  </div>;
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(
+      "https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&q=animals"
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  return (
+    <div className="App">
+     {data && <MainPage mockData={data}></MainPage>}
+    </div>
+  );
 }
 
 export default App;
