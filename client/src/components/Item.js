114825e67838d26e98c610e87183e79b3beeebef
Dima <benensondimitrii@gmail.com>
2022-04-25 17:00:44 +0300

    added modal for items

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,12 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 const Item = ({ item }) => {
-  const { tags, previewURL } = item;
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const { tags, previewURL, views, downloads, collections } = item;
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div>
-      <h1>{tags}</h1>
-      <img src={previewURL} alt="img"></img>
+      <button onClick={openModal}>
+        <h1>{tags}</h1>
+        <img src={previewURL} alt="img"></img>
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="test"
+        ariaHideApp={false}
+      >
+        <p>views: {views}</p>
+        <p>downloads: {downloads}</p>
+        <p>collections: {collections}</p>
+      </Modal>
     </div>
   );
 };
