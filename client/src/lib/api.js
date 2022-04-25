fa6e6799c84f502e3bcfa42b3949e8c3b15edcc7
Dima <benensondimitrii@gmail.com>
2022-04-25 18:53:04 +0300

    added react select component

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BaseUrl = 'https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&q=animals';
 
-export async function getData(token) {
+export async function getData() {
     const response = await axios.get(`${BaseUrl}`);
     return response.data
 }
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,9 +1,21 @@
 import React from "react";
 import List from "../components/List";
+import Select from "react-select";
 
 const MainPage = ({ mockData }) => {
   const { hits } = mockData;
-  return <List listData={hits}></List>;
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
+  return (
+    <div>
+      <Select options={options} />
+      <List listData={hits}></List>;
+    </div>
+  );
 };
 
 export default MainPage;
