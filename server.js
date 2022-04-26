fc300b95a6064ad0c3b0aa396e2d58373ac611b5
Dima <benensondimitrii@gmail.com>
2022-04-26 16:49:08 +0300

    update server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const getData = require("./routes/getData.js")
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.use("/data", getData)
 
 const port = 5001
 const host = "0.0.0.0"
