8be455fb93e5768c521ee18656fb54ab5b64cbe6
Dima <benensondimitrii@gmail.com>
2022-04-27 18:03:51 +0300

    update api front

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,17 +1,6 @@
 import axios from "axios";
 
-// const BaseUrl =
-//   "https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&q";
-
-// export async function getData(option) {
-//   const response = await axios.get(`${BaseUrl}=${option}`);
-//   return response.data;
-// }
-
-
 export async function getData(option) {
-  console.log("get route front");
-  const response = await axios.get(`http://0.0.0.0:5001/data${option}`);
-  console.log(response.data);
-  return response.data;
+  const response = await axios.get(`http://0.0.0.0:5001/data/${option}`);
+  return response.data.data;
 }
