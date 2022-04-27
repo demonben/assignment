a6c53b07a83aac200093e804d6fc2f0fee69c470
Dima <benensondimitrii@gmail.com>
2022-04-27 16:42:00 +0300

    minor server fix and added axios

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
-const getData = require("./routes/getData.js")
+const dataRouter = require("./routes/dataRouter.js")
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-app.use("/data", getData)
+app.use("/data", dataRouter)
 
 const port = 5001
 const host = "0.0.0.0"
 app.listen(port, host, () => {
     console.log(`The server is listening on http://${host}:${port}`);
   });
+
