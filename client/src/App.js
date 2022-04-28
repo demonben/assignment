f397fc0fcfedc2dae850b7c3f3c8d7218465f2b8
Dima <benensondimitrii@gmail.com>
2022-04-28 19:51:07 +0300

    created redux and store for fetch data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import MainPage from "./pages/MainPage";
-
+import { useSelector, useDispatch } from "react-redux";
+import { getDataAsync } from "./redux/dataSlice";
 import { getData } from "./lib/api";
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
   const [data, setData] = useState();
   const [option, setOption] = useState("animals");
+  const dataFromStore = useSelector((state) => state.data);
 
   const options = [
     { value: "animals", label: "animals" },
@@ -16,10 +19,11 @@ function App() {
   const selectHandler = (e) => {
     setOption(e.value);
   };
-  
+
   useEffect(() => {
+    dispatch(getDataAsync());
     getData(option).then((data) => setData(data));
-  }, [option]);
+  }, [option, dispatch]);
 
   return (
     <div className="App">
