aa69e3d8d8a78ebdcf9739d01ef92dd68c85f8c2
Dima <benensondimitrii@gmail.com>
2022-05-03 15:27:10 +0300

    added loader component and logic for next and prev but

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,22 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setData } from "../redux/actions/dataActions";
 import List from "../components/List";
 import Select from "react-select";
+import { Hearts } from "react-loader-spinner";
 
 const MainPage = ({ options, selectHandler, page, setPage }) => {
+  const [isLoading, setLoading] = useState(false);
   const dispatch = useDispatch();
   let optionDefault = "animals";
 
   const setInStoreGetedData = async (option, page) => {
+    setLoading(true);
+
     const response = await axios.get(
       `http://0.0.0.0:5001/data/${option}/${page}`
     );
     dispatch(setData(response.data));
+    setLoading(false);
   };
   useEffect(() => {
-    setInStoreGetedData(optionDefault, 56);
+    setInStoreGetedData(optionDefault, page);
   }, [page]);
 
   const nextPageHandler = () => {
@@ -29,8 +34,17 @@ const MainPage = ({ options, selectHandler, page, setPage }) => {
     <div>
       <Select options={options} onChange={selectHandler} />
       <List />
-      {page <= 55 && <button onClick={nextPageHandler}>NEXT</button>}
-      {page > 1 && <button onClick={prevPageHandler}>PREV</button>}
+      <div >
+        {page <= 55 && !isLoading && (
+          <button onClick={nextPageHandler}>NEXT</button>
+        )}
+        {page > 1 && !isLoading && (
+          <button onClick={prevPageHandler}>PREV</button>
+        )}
+        <div className="loader">
+        {isLoading && <Hearts color="red" height={80} width={80} />}
+        </div>
+      </div>
     </div>
   );
 };
