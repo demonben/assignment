7dbe99beeaabf471df84c025c67f0272a91d6ddc
Dima <benensondimitrii@gmail.com>
2022-05-04 12:06:01 +0300

    added loader in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import MainPage from "./pages/MainPage";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { setData } from "./redux/actions/dataActions";
+import { setData, setIsFetching } from "./redux/actions/dataActions";
+import { Hearts } from "react-loader-spinner";
 
 function App() {
   const [option, setOption] = useState("animals");
+  const isFetching = useSelector((state) => state.store.isFetching);
+
   const [page, setPage] = useState(1);
   const dispatch = useDispatch();
 
@@ -18,10 +21,17 @@ function App() {
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const setInStoreGetedData = async (option, page) => {
-    const response = await axios.get(
-      `http://0.0.0.0:5001/data/${option}/${page}`
-    );
-    dispatch(setData(response.data));
+    try {
+      dispatch(setIsFetching(true));
+      const response = await axios.get(
+        `http://0.0.0.0:5001/data/${option}/${page}`
+      );
+
+      dispatch(setData(response.data));
+      dispatch(setIsFetching(false));
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const selectHandler = (e) => {
@@ -29,16 +39,20 @@ function App() {
   };
   useEffect(() => {
     setInStoreGetedData(option, page);
-  }, [option, page, setInStoreGetedData]);
+  }, [option]);
 
   return (
     <div className="App">
-      <MainPage
-        options={options}
-        selectHandler={selectHandler}
-        page={page}
-        setPage={setPage}
-      ></MainPage>
+      {isFetching ? (
+        <Hearts color="red" height={80} width={80} />
+      ) : (
+        <MainPage
+          options={options}
+          selectHandler={selectHandler}
+          page={page}
+          setPage={setPage}
+        ></MainPage>
+      )}
     </div>
   );
 }
