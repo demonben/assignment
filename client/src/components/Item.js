100a443ee092b4a4dd0db5db52b1f9c95e6441f0
Dima <benensondimitrii@gmail.com>
2022-05-04 15:27:59 +0300

    update cards styles

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import Modal from "react-modal";
 
 const Item = ({ item }) => {
@@ -15,23 +21,50 @@ const Item = ({ item }) => {
 
   return (
     <div className="listItem">
-      <button onClick={openModal} className="moduleItemBtn">
-        <h1>{tags}</h1>
-        <img src={previewURL} alt="img"></img>
-      </button>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="test"
-        ariaHideApp={false}
-      >
-        <p>views: {views}</p>
-        <p>downloads: {downloads}</p>
-        <p>collections: {collections}</p>
-        <img src={webformatURL} alt="no web format img"></img>
-      </Modal>
+      <Card sx={{ maxWidth: 250 }}>
+        <CardMedia
+          component="img"
+          alt="photo"
+          height="300"
+          image={previewURL}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {tags}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={openModal}>
+            Learn More
+          </Button>
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="test"
+            ariaHideApp={false}
+          >
+            
+          </Modal>
+        </CardActions>
+      </Card>
     </div>
   );
 };
-
+// <div className="listItem">
+//   <button onClick={openModal} className="moduleItemBtn">
+//     <h1>{tags}</h1>
+//     <img src={previewURL} alt="img"></img>
+//   </button>
+//   <Modal
+//     isOpen={modalIsOpen}
+//     onRequestClose={closeModal}
+//     contentLabel="test"
+//     ariaHideApp={false}
+//   >
+//     <p>views: {views}</p>
+//     <p>downloads: {downloads}</p>
+//     <p>collections: {collections}</p>
+//     <img src={webformatURL} alt="no web format img"></img>
+//   </Modal>
+// </div>
 export default Item;
