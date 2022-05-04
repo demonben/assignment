2713277e51e202443313f451f8ceefc947381d10
Dima <benensondimitrii@gmail.com>
2022-05-04 12:30:47 +0300

    fixed naming in store for set page

--- client/src/redux/reducers/dataReducer.js
+++ client/src/redux/reducers/dataReducer.js
@@ -3,6 +3,7 @@ import { ActionTypes } from "../constants/action-types";
 const intitialState = {
   data: [],
   isFetching: false,
+  page: 1,
 };
 
 export const dataReducer = (state = intitialState, { type, payload }) => {
@@ -11,6 +12,8 @@ export const dataReducer = (state = intitialState, { type, payload }) => {
       return { ...state, data: payload };
     case ActionTypes.SET_IS_FETCHING:
       return { ...state, isFetching: payload };
+    case ActionTypes.SET_CURRENT_PAGE:
+      return { ...state, page: payload };
     default:
       return state;
   }
