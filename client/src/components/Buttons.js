8f6f63abf5ba9702e9f99340ae6be05705218115
Dima <benensondimitrii@gmail.com>
2022-05-04 12:56:34 +0300

    added more logic for buttons component

--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -1,32 +1,43 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { setPageNumber } from "../redux/actions/dataActions";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setPageNumber, setData } from "../redux/actions/dataActions";
 
 const Buttons = () => {
   const page = useSelector((state) => state.store.page);
   const dispatch = useDispatch();
+  let optionDefault = "animals";
 
   let FIRST_PAGE = 1;
   let LAST_PAGE = 55;
+  // TODO DO FETCH REQ AND SET NEW DATA
+  // TODO REFACTOR FETCH REQ NOT FOLLOW DRY
 
   const nextPageHandler = () => {
     dispatch(setPageNumber(page + 1));
+    setInStoreGetedData(optionDefault, page);
   };
   const prevPageHandler = () => {
-    dispatch(setPageNumber(page - 1))
+    dispatch(setPageNumber(page - 1));
+    setInStoreGetedData(optionDefault, page);
+  };
+
+  const setInStoreGetedData = async (option, page) => {
+    try {
+      const response = await axios.get(
+        `http://0.0.0.0:5001/data/${option}/${page}`
+      );
+      dispatch(setData(response.data));
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>
-      <button onClick={nextPageHandler}>NEXT</button>
-      <button onClick={prevPageHandler}>PREV</button>
+      {page <= LAST_PAGE && <button onClick={nextPageHandler}>NEXT</button>}
+      {page > FIRST_PAGE && <button onClick={prevPageHandler}>PREV</button>}
     </div>
   );
 };
-      {/* {page <= lastPage && !isLoading && (
-          <button onClick={nextPageHandler}>NEXT</button>
-        )}
-        {page > firstPage && !isLoading && (
-          <button onClick={prevPageHandler}>PREV</button>
-        )} */}
+
 export default Buttons;
