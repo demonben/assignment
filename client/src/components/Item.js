6cee42296498563b74eb644bab85d2f3d4108419
Dima <benensondimitrii@gmail.com>
2022-05-06 10:43:17 +0300

    removed comments

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -78,21 +78,4 @@ const Item = ({ item }) => {
     </div>
   );
 };
-// <div className="listItem">
-//   <button onClick={openModal} className="moduleItemBtn">
-//     <h1>{tags}</h1>
-//     <img src={previewURL} alt="img"></img>
-//   </button>
-//   <Modal
-//     isOpen={modalIsOpen}
-//     onRequestClose={closeModal}
-//     contentLabel="test"
-//     ariaHideApp={false}
-//   >
-//     <p>views: {views}</p>
-//     <p>downloads: {downloads}</p>
-//     <p>collections: {collections}</p>
-//     <img src={webformatURL} alt="no web format img"></img>
-//   </Modal>
-// </div>
 export default Item;
